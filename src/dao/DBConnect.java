96ed820ea640f2add4ed0c5328e0c1dd169bfa06
MinhQuan <minhquanmsi2000@gmail.com>
2021-05-28 17:04:59 +0700

    task

---
+++ src/dao/DBConnect.java
@@ -0,0 +1,60 @@
+
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DBConnect {
+    private static String DB_URL = "jdbc:mysql://localhost:8080/taskdb";
+    private static String USER_NAME = "root";
+    private static String PASSWORD = "1234567890";
+     public static void main(String args[]) {
+        try {
+            // connnect to database 'testdb'
+            Connection conn = getConnection(DB_URL, USER_NAME, PASSWORD);
+            // crate statement
+            Statement stmt = conn.createStatement();
+            // get data from table 'student'
+            ResultSet rs = stmt.executeQuery("select * from task");
+            // show data
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) 
+                        + "  " + rs.getString(3));
+            }
+            // close connection
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+ 
+ 
+    /**
+     * create connection 
+     * 
+     * @author viettuts.vn
+     * @param dbURL: database's url
+     * @param userName: username is used to login
+     * @param password: password is used to login
+     * @return connection
+     */
+    public static Connection getConnection(String dbURL, String userName, 
+            String password) {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(dbURL, userName, password);
+            System.out.println("connect successfully!");
+        } catch (Exception ex) {
+            System.out.println("connect failure!");
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+}
